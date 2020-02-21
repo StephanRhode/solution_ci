d607d62a8525d07638390640e6b007403431d850
StephanRhode <none>
2020-02-21 14:18:00 +0100

    Adds module for CI
    Signed-off-by: StephanRhode <none>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # solution_ci
-This repository is the solution to exercise continuous integration in https://github.com/StephanRhode/py-algorithms-4-automotive-engineering
+This repository is the solution to exercise continuous integration in 
+https://github.com/StephanRhode/py-algorithms-4-automotive-engineering
---
+++ tests/test_my_source.py
@@ -0,0 +1,19 @@
+from pytest import approx
+import math
+from my_source import add_func, euclid
+
+
+def test_add_one():
+    assert add_func(2, 5) == 7
+
+
+@pytest.mark.xfail
+def test_add_fail():
+    assert add_func(5, 5) == 9
+
+
+def test_euclid():
+    a = [0, 0, 0]
+    b = [4, 4, 4]
+    dist = euclid(a, b)
+    assert(math.sqrt(48.) == approx(dist))
